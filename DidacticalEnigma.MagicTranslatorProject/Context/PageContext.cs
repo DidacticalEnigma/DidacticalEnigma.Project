bb895240c0346d6c525ca3f6db556a4c16b2eb6a
milleniumbug <milleniumbug@protonmail.com>
2021-05-29 20:21:06 +0200

    Update Magical Translator project handling

--- DidacticalEnigma.MagicTranslatorProject/Context/PageContext.cs
+++ DidacticalEnigma.MagicTranslatorProject/Context/PageContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using DidacticalEnigma.Project;
@@ -18,25 +19,42 @@ namespace MagicTranslatorProject.Context
         internal PageContext([NotNull] MangaContext root, [NotNull] ProjectDirectoryListingProvider listing, [NotNull] PageId page)
         {
             this.name = root.Name;
+            this.listing = listing;
             this.page = page;
-            var pageJson = JsonConvert.DeserializeObject<PageJson>(File.ReadAllText(listing.GetCapturePath(page)), new CharacterTypeConverter(root.IdNameMapping));
-            captures = pageJson.Captures
-                .Select((c, i) =>
-                {
-                    var guid = root.Map(page, c.Id).Some();
-                    return new CaptureContext(this, c, j =>
+            try
+            {
+                var pageJson = JsonConvert.DeserializeObject<PageJson>(File.ReadAllText(listing.GetCapturePath(page)),
+                    new CharacterTypeConverter(root.IdNameMapping));
+                captures = pageJson.Captures
+                    .Select((c, i) =>
                     {
-                        pageJson.Captures[i] = j;
-                        return ModificationResult.WithSuccess(new Translation(c, guid));
-                    }, new Translation(c, guid));
-                })
-                .ToList();
+                        var guid = root.Map(page, c.Id).Some();
+                        return new CaptureContext(this, c, j =>
+                        {
+                            pageJson.Captures[i] = j;
+                            return ModificationResult.WithSuccess(new Translation(c, guid));
+                        }, new Translation(c, guid));
+                    })
+                    .ToList();
+            }
+            catch (FileNotFoundException)
+            {
+                this.captures = Array.Empty<CaptureContext>();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                this.captures = Array.Empty<CaptureContext>();
+            }
         }
+
+        public string PathToRaw => this.listing.GetRawPath(page);
 
         private readonly IList<CaptureContext> captures;
 
         private readonly string name;
 
+        [NotNull] private readonly ProjectDirectoryListingProvider listing;
+        
         private PageId page;
 
         public IEnumerable<CaptureContext> Children => captures;
